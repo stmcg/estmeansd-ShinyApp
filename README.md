5285fa129fba9b4c09fef5fe58eea829c13b9dbd
Sean McGrath <smcgrath@hsph.harvard.edu>
2022-05-28 14:19:56 -1000

    Added the MLN approach as well as the bootstrap SE estimators

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # estmeansd-ShinyApp: Shiny App for the estmeansd R package
 
-A Shiny App (launched [here](https://smcgrath.shinyapps.io/estmeansd)) applying the approaches of [McGrath et al. (2020)](https://journals.sagepub.com/doi/full/10.1177/0962280219889080) for estimating the sample mean and standard deviation from the sample median and one or both of (i) the minimum and maximum values and (ii) the first and third quartiles.
+This Shiny App (launched [here](https://smcgrath.shinyapps.io/estmeansd)) applies the approaches of [McGrath et al. (2020)](https://journals.sagepub.com/doi/full/10.1177/0962280219889080) and [Cai et al. (2021)](https://journals.sagepub.com/doi/full/10.1177/09622802211047348) for estimating the sample mean and standard deviation from the sample median and one or both of (i) the minimum and maximum values and (ii) the first and third quartiles. This app also applies the corresponding SE estimators based on the parametric bootstrap approach of McGrath et al. (In preparation).
 
 The corresponding R package is `estmeansd`, available on [CRAN](https://CRAN.R-project.org/package=estmeansd) and [GitHub](https://github.com/stmcg/estmeansd). 
--- app.R
+++ app.R
@@ -4,14 +4,16 @@ library(estmeansd)
 ui <- fluidPage(
   titlePanel("Estimating the sample mean and standard deviation"),
 
-  HTML(paste0("This webpage applies the either the quantile estimation (QE) method or the Box-Cox (BC) method of ", tags$a(href="https://journals.sagepub.com/doi/full/10.1177/0962280219889080", "McGrath et al. (2020)"), " to estimate the sample mean and standard deviation from a study that presents one of the following sets of summary statistics:")),
+  HTML(paste0("This webpage applies the quantile estimation (QE) method and the Box-Cox (BC) method of ", tags$a(href="https://journals.sagepub.com/doi/full/10.1177/0962280219889080", "McGrath et al. (2020)"), " as well as the Method for Unknown Non-Normal Distributions (MLN) approach of ", tags$a(href="https://journals.sagepub.com/doi/full/10.1177/09622802211047348", "Cai et al. (2021)"), " to estimate the sample mean and standard deviation from a study that presents one of the following sets of summary statistics:")),
 
   tags$ul(
     tags$li("S1: median, minimum and maximum values, and sample size"),
     tags$li("S2: median, first and third quartiles, and sample size"),
     tags$li("S3: median, minimum and maximum values, first and third quartiles, and sample size")
   ),
-
+  HTML(paste0("The standard error (SE) of the mean is estimated by the parametric bootstrap approach of McGrath et al. (In preparation).")),
+  br(), br(), 
+  
   HTML(paste0("These methods are also implemented in the R package ", tags$a(href="https://CRAN.R-project.org/package=estmeansd", "estmeansd"), ", which provides detailed documentation and greater flexibility for applying these methods.")),
   br(),
   br(),
@@ -27,19 +29,27 @@ ui <- fluidPage(
                  numericInput("q3.val", "Third quartile value (if available)", value = NA),
                  numericInput("max.val", "Maximum value (if available)", value = NA),
                  numericInput("n", "Sample size", value = NA, min = 2, max=1e6),
+                 numericInput("nboot", "Number of bootstrap samples (for estimating the SE)", value = 1e3, min = 2, max=1e5),
                  radioButtons("method.name", "Method",
-                              choices = c("Quantile Estimation (QE)", "Box-Cox (BC)"),
+                              choices = c("Quantile Estimation (QE)", "Box-Cox (BC)", "Method for Unknown Non-Normal Distributions (MLN)"),
                               selected = "Quantile Estimation (QE)")),
     mainPanel(
       uiOutput(outputId = "est_mean"),
       br(),
-      uiOutput(outputId = "est_sd")
+      uiOutput(outputId = "est_sd"), 
+      br(),
+      uiOutput(outputId = "est_se")
     )
   ),
   hr(),
   tags$h4("Reference:"),
   print("McGrath S., Zhao X., Steele R., Thombs B.D., Benedetti A., and the DEPRESsion Screening Data (DEPRESSD) Collaboration. (2020). Estimating the sample mean and standard deviation from commonly reported quantiles in meta-analysis. Statistical Methods in Medical Research, 29(9), 2520–2537."),
-  HTML(paste0(tags$a(href="https://journals.sagepub.com/doi/full/10.1177/0962280219889080", "https://journals.sagepub.com/doi/full/10.1177/0962280219889080")))
+  HTML(paste0(tags$a(href="https://journals.sagepub.com/doi/full/10.1177/0962280219889080", "https://journals.sagepub.com/doi/full/10.1177/0962280219889080"))), 
+  br(), br(),
+  print("Cai S., Zhou J., and Pan J. (2021). Estimating the sample mean and standard deviation from order statistics and sample size in meta-analysis. Statistical Methods in Medical Research, 30(12), 2701-2719."),
+  HTML(paste0(tags$a(href="https://journals.sagepub.com/doi/full/10.1177/09622802211047348", "https://journals.sagepub.com/doi/full/10.1177/09622802211047348"))), 
+  br(), br(),
+  print("McGrath S., Katzenschlager S., Zimmer A.J., Seitel A., Steele R., and Benedetti A. (2022). Standard error estimation in meta-analysis of studies reporting medians. In preparation"), 
 )
 
 server <- function(input, output) {
@@ -50,16 +60,27 @@ server <- function(input, output) {
   myreact <- reactive({
     set.seed(1)
     if(input$method.name == "Quantile Estimation (QE)"){
-      tryCatch({
+      est <- tryCatch({
         qe.mean.sd(min.val = input$min.val, q1.val = input$q1.val, med.val = input$med.val,
                    q3.val = input$q3.val, max.val = input$max.val, n = input$n)
-      }, error = no.fit, warning = no.fit)
+        }, error = no.fit, warning = no.fit)
     } else if (input$method.name == "Box-Cox (BC)"){
-      tryCatch({
+      est <- tryCatch({
         bc.mean.sd(min.val = input$min.val, q1.val = input$q1.val, med.val = input$med.val,
                    q3.val = input$q3.val, max.val = input$max.val, n = input$n)
-      }, error = no.fit, warning = no.fit)
+        }, error = no.fit, warning = no.fit)
+    } else if (input$method.name == "Method for Unknown Non-Normal Distributions (MLN)"){
+      est <- tryCatch({
+        mln.mean.sd(min.val = input$min.val, q1.val = input$q1.val, med.val = input$med.val,
+                    q3.val = input$q3.val, max.val = input$max.val, n = input$n)
+        }, error = no.fit, warning = no.fit)
     }
+    if (class(est) %in% c('qe.mean.sd', 'bc.mean.sd', 'mln.mean.sd')){
+      SE <- get_SE(est, nboot = input$nboot)$est.se
+    } else {
+      SE <- NA
+    }
+    return(list(est.mean = est$est.mean, est.sd = est$est.sd, est.se = SE))
   })
 
 
@@ -68,6 +89,9 @@ server <- function(input, output) {
   })
   output$est_sd <- renderText({
     HTML(paste("<b>", "Estimated standard deviation:", sprintf('%.2f', round(myreact()$est.sd, 2)), "</b>"))
+  })
+  output$est_se <- renderText({
+    HTML(paste("<b>", "Estimated standard error of the mean:", sprintf('%.2f', round(myreact()$est.se, 2)), "</b>"))
   })
 
 }
