72b8bddcd6e9e5c7c6111f974ea82d9a28b11ace
Sean McGrath <smcgrath@hsph.harvard.edu>
2023-12-13 13:34:39 -0500

    Updated references

--- app.R
+++ app.R
@@ -4,18 +4,21 @@ library(estmeansd)
 ui <- fluidPage(
   titlePanel("Estimating the sample mean and standard deviation"),
 
-  HTML(paste0("This webpage applies the quantile estimation (QE) method and the Box-Cox (BC) method of ", tags$a(href="https://journals.sagepub.com/doi/full/10.1177/0962280219889080", "McGrath et al. (2020)"), " as well as the Method for Unknown Non-Normal Distributions (MLN) approach of ", tags$a(href="https://journals.sagepub.com/doi/full/10.1177/09622802211047348", "Cai et al. (2021)"), " to estimate the sample mean and standard deviation from a study that presents one of the following sets of summary statistics:")),
+  HTML(paste0("This webpage applies the quantile estimation (QE) method and the Box-Cox (BC) method of ", tags$a(href="https://doi.org/10.1177/0962280219889080", "McGrath et al. (2020)"), " as well as the Method for Unknown Non-Normal Distributions (MLN) approach of ", tags$a(href="https://doi.org/10.1177/09622802211047348", "Cai et al. (2021)"), " to estimate the sample mean and standard deviation from a study that presents one of the following sets of summary statistics:")),
 
   tags$ul(
     tags$li("S1: median, minimum and maximum values, and sample size"),
     tags$li("S2: median, first and third quartiles, and sample size"),
     tags$li("S3: median, minimum and maximum values, first and third quartiles, and sample size")
   ),
-  HTML(paste0("The standard error (SE) of the mean is estimated by the parametric bootstrap approach of ", tags$a(href="https://arxiv.org/abs/2206.14386", "McGrath et al. (in press)"), ".")),
+  HTML(paste0("The standard error (SE) of the mean is estimated by the parametric bootstrap approach of ", tags$a(href="https://doi.org/10.1177/09622802221139233", "McGrath et al. (2023)"), ".")),
   br(), br(), 
   
   HTML(paste0("These methods are also implemented in the R package ", tags$a(href="https://CRAN.R-project.org/package=estmeansd", "estmeansd"), ", which provides detailed documentation and greater flexibility for applying these methods.")),
   br(),
+  br(),
+  HTML(paste0("Note that the R package ", tags$a(href="https://CRAN.R-project.org/package=metamedian", "metamedian"), " can apply these methods (as well as several others) to perform a meta-analysis. See ", tags$a(href="https://doi.org/10.1002/jrsm.1686", "McGrath et al. (in press)"),  " for a guide on using the metamedian package.")), 
+  br(), 
   br(),
 
   tags$i("If the summary statistics are not in the appropriate form (i.e., S1, S2, or S3), values of NA will be given for the estimated sample mean and standard deviation."),
@@ -43,14 +46,17 @@ ui <- fluidPage(
   ),
   hr(),
   tags$h4("References:"),
-  print("McGrath S., Zhao X., Steele R., Thombs B.D., Benedetti A., and the DEPRESsion Screening Data (DEPRESSD) Collaboration. (2020). Estimating the sample mean and standard deviation from commonly reported quantiles in meta-analysis. Statistical Methods in Medical Research, 29(9), 2520–2537."),
-  HTML(paste0(tags$a(href="https://journals.sagepub.com/doi/full/10.1177/0962280219889080", "https://journals.sagepub.com/doi/full/10.1177/0962280219889080"))), 
-  br(), br(),
   print("Cai S., Zhou J., and Pan J. (2021). Estimating the sample mean and standard deviation from order statistics and sample size in meta-analysis. Statistical Methods in Medical Research, 30(12), 2701-2719."),
-  HTML(paste0(tags$a(href="https://journals.sagepub.com/doi/full/10.1177/09622802211047348", "https://journals.sagepub.com/doi/full/10.1177/09622802211047348"))), 
+  HTML(paste0(tags$a(href="https://doi.org/10.1177/09622802211047348", "https://doi.org/10.1177/09622802211047348"))), 
   br(), br(),
-  print("McGrath S., Katzenschlager S., Zimmer A.J., Seitel A., Steele R., and Benedetti A. (In press). Standard error estimation in meta-analysis of studies reporting medians. Statistical Methods in Medical Research."), 
-  HTML(paste0(tags$a(href="https://arxiv.org/abs/2206.14386", "https://arxiv.org/abs/2206.14386"))), 
+  print("McGrath S., Zhao X., Steele R., Thombs B.D., Benedetti A., and the DEPRESsion Screening Data (DEPRESSD) Collaboration. (2020). Estimating the sample mean and standard deviation from commonly reported quantiles in meta-analysis. Statistical Methods in Medical Research, 29(9), 2520–2537."),
+  HTML(paste0(tags$a(href="https://doi.org/10.1177/0962280219889080", "https://doi.org/10.1177/0962280219889080"))), 
+  br(), br(),
+  print("McGrath S., Katzenschlager S., Zimmer A.J., Seitel A., Steele R., and Benedetti A. (2023). Standard error estimation in meta-analysis of studies reporting medians. Statistical Methods in Medical Research, 32(2), 373-388."), 
+  HTML(paste0(tags$a(href="https://doi.org/10.1177/09622802221139233", "https://doi.org/10.1177/09622802221139233"))), 
+  br(), br(),
+  print("McGrath S., Zhao X., Ozturk O., Katzenschlager S., Steele R., and Benedetti A. (in press). metamedian: An R package for meta-analyzing studies reporting medians. Research Synthesis Methods."), 
+  HTML(paste0(tags$a(href="https://doi.org/10.1002/jrsm.1686", "https://doi.org/10.1002/jrsm.1686"))), 
 )
 
 server <- function(input, output) {
